c0470053210fd75ca6e8d7545eb30db5d4c95d9d
Dustin Jenkins <djenkins.cadc@gmail.com>
2019-06-14 10:48:58 -0700

    Added configurable cookie timeout.

--- cadc-web-util/src/main/java/ca/nrc/cadc/accesscontrol/AccessControlUtil.java
+++ cadc-web-util/src/main/java/ca/nrc/cadc/accesscontrol/AccessControlUtil.java
@@ -3,65 +3,64 @@ package ca.nrc.cadc.accesscontrol;
 import ca.nrc.cadc.config.ApplicationConfiguration;
 import ca.nrc.cadc.util.StringUtil;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
 
 
-public class AccessControlUtil
-{
+public class AccessControlUtil {
+
     private static final Logger LOG = Logger.getLogger(AccessControlUtil.class);
     public static final String SSO_COOKIE_NAME = "CADC_SSO";
     static final String SSO_SERVERS_KEY = "SSO_SERVERS";
+    public static final String SSO_COOKIE_LIFETIME_SECONDS_KEY = "SSO_TOKEN_LIFETIME_SECONDS";
     public static final String DEFAULT_AC_PROPERTIES_FILE_PATH =
             System.getProperty("user.home") + "/config/AccessControl.properties";
+    public static final int DEFAULT_COOKIE_LIFETIME_SECONDS = 48 * 60 * 60; // 48 hours
     private final ApplicationConfiguration applicationConfiguration;
 
 
-    public AccessControlUtil(ApplicationConfiguration applicationConfiguration)
-    {
+    public AccessControlUtil(ApplicationConfiguration applicationConfiguration) {
         this.applicationConfiguration = applicationConfiguration;
     }
 
     /**
      * Load from the given file path as well as the System properties.
-     * @param filePath      Path to a known configuration file.
+     *
+     * @param filePath Path to a known configuration file.
      */
-    public AccessControlUtil(final String filePath)
-    {
+    public AccessControlUtil(final String filePath) {
         this(new ApplicationConfiguration(filePath));
     }
 
     /**
      * Load from the default file path.
      */
-    public AccessControlUtil()
-    {
+    public AccessControlUtil() {
         this(DEFAULT_AC_PROPERTIES_FILE_PATH);
     }
 
 
-    public Set<String> getSSOServers()
-    {
+    public Set<String> getSSOServers() {
         final Set<String> servers = new HashSet<>();
-        final String hostsString =
-                applicationConfiguration.lookup(SSO_SERVERS_KEY);
+        final String hostsString = applicationConfiguration.lookup(SSO_SERVERS_KEY);
 
-        if (StringUtil.hasText(hostsString))
-        {
+        if (StringUtil.hasText(hostsString)) {
             final String[] hosts = hostsString.split(" ");
 
-            for (final String host : hosts)
-            {
-                if (StringUtil.hasLength(host))
-                {
+            for (final String host : hosts) {
+                if (StringUtil.hasLength(host)) {
                     servers.add(host);
                 }
             }
         }
 
         return servers;
+    }
+
+    public int getCookieLifetimeSeconds() {
+        return applicationConfiguration.lookupInt(AccessControlUtil.SSO_COOKIE_LIFETIME_SECONDS_KEY,
+                                                  AccessControlUtil.DEFAULT_COOKIE_LIFETIME_SECONDS);
     }
 }
